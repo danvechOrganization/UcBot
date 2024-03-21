cff6d996f2360b752e98050b3091d633e991a4d8
IvIedweD <pestriy00@mail.eu>
2024-03-21 13:00:31 +0600

    Отдельные файлы для UA и proxy. Убрал повторение proxy

--- tgbot/services/chromiumdriver.py
+++ tgbot/services/chromiumdriver.py
@@ -1,46 +1,34 @@
 import random
 from selenium import webdriver
 
-proxy_list = [
-    '185.39.148.176:8000',
-    '185.39.148.222:8000',
-    '46.161.20.122:8000',
-    '46.161.21.139:8000',
-    '46.161.21.242:8000',
-    '46.161.21.218:8000',
-    '46.161.20.148:8000',
-]
+from tgbot.services.ProxyAndUA.proxy_list import proxy_list
+from tgbot.services.ProxyAndUA.ua_list import user_agent_list
 
-user_agent_list = [
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.5845.111 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/118.0',
-    'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0',
-    'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 OPR/98.0.0.0 (Edition beta)',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 OPR/102.0.0.0',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 OPR/101.0.0.0 (Edition Yx 03)',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.5790.171 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Iron Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0',
-    'Mozilla/5.0 (Windows NT 10.0; rv:109.0) Gecko/20100101 Firefox/116.0',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/116.0',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 OPR/101.0.0.0 (Edition Yx 05)',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 OPR/101.0.0.0 (Edition Campaign 34)',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 OPR/101.0.0.0 (Edition Yx GX)',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5759.201 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; WOW64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5748.222 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-]
+previous_proxy = None  # Переменная для хранения предыдущего прокси
 
 def get_chromedriver(proxy_list=None, user_agent_list=None):
+    global previous_proxy  # Объявление глобальной переменной
     options = webdriver.ChromeOptions()
     options.add_experimental_option("detach", True)
 
     if proxy_list:
+        # Удаляем предыдущий прокси из списка
+        if previous_proxy in proxy_list:
+            proxy_list.remove(previous_proxy)
+        # Если все прокси были использованы, сбрасываем список
+        if len(proxy_list) == 0:
+            proxy_list = [
+                '185.39.148.176:8000',
+                '185.39.148.222:8000',
+                '46.161.20.122:8000',
+                '46.161.21.139:8000',
+                '46.161.21.242:8000',
+                '46.161.21.218:8000',
+                '46.161.20.148:8000',
+            ]
+        # Выбираем новый прокси случайным образом
         proxy = random.sample(proxy_list, 1)[0]
+        previous_proxy = proxy  # Обновляем значение previous_proxy
         options.add_argument(f'--proxy-server={proxy}')
 
     if user_agent_list:
